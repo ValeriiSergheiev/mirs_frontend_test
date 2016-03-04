605b0be432adfcb443dbfbafff0b88992a6616c9
ValeriiSergheiev <voodoo.liamg@gmail.com>
2016-03-05 01:42:02 +0200

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('.carousel').carousel({
+    interval: 4000
+  });
+
+  $('#country').selectmenu({
+    icons: { button: " ui-icon-triangle-1-s" }
+  });
+
+});
